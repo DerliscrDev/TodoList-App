9ff795ef50cbf114cc3dd72927524e49f1c6545f
Derlis Cañete <derliscrdev@gmail.com>
2023-04-12 22:36:16 -0400

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Derlis Ramón Cañete Ríos on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
